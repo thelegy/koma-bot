48291f2b62249a68e578b3c8fc4326542affd22f
Maximilian Marx <mmarx@wh2.tu-dresden.de>
2015-11-28 19:43:04 +0100

    Add first trigger test cases

--- tests/test_trigger.py
+++ tests/test_trigger.py
@@ -1,6 +1,8 @@
 from koma_bot import actions_for
+from app import WithApp
 
 
 class TestTriggers(WithApp):
-    def test_action():
-        pass
+    def test_action(self):
+        self.assertEquals(actions_for('roman'), ['roman'])
+        self.assertEquals(actions_for('oh, man'), [])
