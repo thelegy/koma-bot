fa46ff4069e0367b3a2e36160be93e7895217f60
Maximilian Marx <mmarx@wh2.tu-dresden.de>
2015-11-28 19:45:48 +0100

    Add more test cases

--- tests/test_trigger.py
+++ tests/test_trigger.py
@@ -6,3 +6,10 @@ class TestTriggers(WithApp):
     def test_action(self):
         self.assertEquals(actions_for('roman'), ['roman'])
         self.assertEquals(actions_for('oh, man'), [])
+
+    def test_overlapping(self):
+        self.assertEquals(actions_for('orgamensch'), ['orga', 'zonk'])
+
+    def test_multiple(self):
+        self.assertEquals(actions_for('game game game'),
+                          ['zonk', 'zonk', 'zonk'])
